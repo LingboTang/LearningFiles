579d9d7ccd77acc88b48c5401465b3574e9efa3f
LingboTang <lingbo19.tang@gmail.com>
2017-03-14 15:40:19 -0600

    Added Attributes Selection example in my file

--- IscoreRegression.java
+++ IscoreRegression.java
@@ -7,7 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.evaluation.RegressionAnalysis;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
 import weka.classifiers.functions.LinearRegression;
 import weka.core.FastVector;
 import weka.core.Instances;
@@ -66,24 +67,36 @@ public class IscoreRegression {
         /*
          * Parse data from arff files
          */
-        BufferedReader datafile = readDataFile("/home/lingbo/Desktop/NewTrain/exactAttributes.arff");
+        BufferedReader datafile = readDataFile("/home/lingbo/Desktop/LearningFiles/Data/exactAttributes.arff");
         Instances data = new Instances(datafile);
         data.setClassIndex(data.numAttributes() - 1);
 
         /*
-         * Build an initial classifier first to get the initial correlation coefficients
+         * Filtering Data, currently I used the ChiSquareSubsetEvaluation
+         * and GreedyStepwise to run a stepwise regression on the data.
+         * This will help us reduce the dimention of original data.
+         */
+        AttributeSelection mySelection = new AttributeSelection();
+        mySelection.setOptions(weka.core.Utils.splitOptions("-E \"weka.attributeSelection.CfsSubsetEval -P 1 -E 1\" -S \"weka.attributeSelection.GreedyStepwise -T -1.7976931348623157E308\" "));
+        mySelection.setInputFormat(data);
+        Instances filteredData = Filter.useFilter(data, mySelection);
+
+        /*
+         * Build our linear regression model with options
+         * Stepwise regression, greedy critiria and threshold settings
+         * Print out the final model we have
          */
 
         LinearRegression silicoRegression = new LinearRegression();
         silicoRegression.setOptions(weka.core.Utils.splitOptions("-S 2 -R 1.0E-8 -additional-stats -num-decimal-places 4"));
-        silicoRegression.buildClassifier(data);
+        silicoRegression.buildClassifier(filteredData);
         System.out.println(silicoRegression);
 
         /*
          * Use cross validate to get more accurate linear regression model
          */
 
-        Instances[][] split = crossValidationSplit(data, 10);
+        Instances[][] split = crossValidationSplit(filteredData, 10);
 
         Instances[] trainingSplits = split[0];
         Instances[] testingSplits = split[1];
